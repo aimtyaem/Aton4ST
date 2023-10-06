8f9c64ac2cf81f4c342d0e45cf4ecb231b9cfaa9
Rupam Barui <rupambarui.17@gmail.com>
2023-10-06 23:48:43 +0530

    added preloader

--- src/Layout.js
+++ src/Layout.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
+import Preloader from "./components/Loader/Preloader";
 import NavBar from "./components/NavBar";
 
 const Layout = () => {
-	return (
+	const [isLoading, setIsLoading] = useState(true);
+	useEffect(() => {
+		const delay = () => {
+			setTimeout(() => {
+				setIsLoading(false);
+			}, 2500);
+		};
+		delay();
+	}, []);
+	return isLoading ? (
+		<Preloader />
+	) : (
 		<div>
 			<NavBar />
 			<main>
